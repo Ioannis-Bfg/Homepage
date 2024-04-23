542aba0dc4d1cbb3b7ade31ba4ba92e66384c7ca
Ioannis Boufogiannis <ioannisboufogiannis@gmail.com>
2024-04-23 16:39:45 +0300

    file path

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
+  mode: "development",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
@@ -16,6 +17,7 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
         type: "asset/resource",
       },
     ],
