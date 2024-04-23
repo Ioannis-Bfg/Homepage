0133da97c96867582ab111a8a442a7143b4cea71
Ioannis Boufogiannis <ioannisboufogiannis@gmail.com>
2024-04-23 16:29:32 +0300

    changed asset management

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import _, { entries } from "lodash";
 import "./styles/style.css";
 import "./styles/reset.css";
+import BattleshipIcon from "./images/battleship.png";
+import "./images/hero.jpg";
+import "./images/layered-waves-haikei.svg";
+import "./images/Library .png";
+import "./images/office.jpg";
+import "./images/patras.jpg";
+import "./images/restaurant.png";
+import "./images/tictactoe.png";
+import "./images/Todo List.png";
+import "./images/wave-haikei.svg";
+import "./images/weatherapp.png";
 
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
